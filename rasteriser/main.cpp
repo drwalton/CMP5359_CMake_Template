f084ae5fc1d9b0d05f74d5983af5a4def731dc09
David R Walton <davidwalton3142@gmail.com>
2024-02-14 13:26:23 +0000

    Add geometry.h, fix main.

--- rasteriser/main.cpp
+++ rasteriser/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-    Model model("../models/stanfordbunny.obj")
+    Model model("../models/stanfordbunny.obj");
     std::cout << "Loaded model with " 
         << model.nverts() << " vertices and " 
         << model.nfaces() << " faces." << std::endl;
